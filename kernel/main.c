f1fca68227c3992e2c15816597221aa0acb52307
zhu-to-u <2528487925@qq.com>
2024-02-28 22:24:12 +0800

    实现自己的打印函数

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,19 @@
-int main()
-{
+#include "../lib/kernel/print.h"
+void main(void){
+    put_char('h');
+    put_char('e');
+    put_char('l');
+    put_char('l');
+    put_char('o');
+    put_char(' ');
+    put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('\n');
+    put_char('h');
+    put_char('\b');
     while(1);
-    return 0;
 }
---
+++ lib/kernel/print.h
@@ -0,0 +1,5 @@
+#ifndef _LIB_KERNEL_PRINT_H
+#define _LIB_KERNEL_PRINT_H
+#include "stdint.h"
+void put_char(uint8_t char_asci);
+#endif
