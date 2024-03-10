1882623a621da29b1e028788bbad1ef428160046
zhu-to-u <2528487925@qq.com>
2024-03-10 11:41:22 +0800

    增加时钟中断部分代码

--- kernel/init.c
+++ kernel/init.c
@@ -1,9 +1,11 @@
 #include "init.h"
 #include "../lib/kernel/print.h"
 #include "interrupt.h"
+#include "../device/timer.h"
 
 //初始化所有模块
 void init_all(){
     put_str("init_all\n");
     idt_init();
+    timer_init();
 }
