08031f16ebafe5449e724b500402521fd52bf23f
zhu-to-u <2528487925@qq.com>
2024-03-13 22:51:20 +0800

    增加链表结构定义

--- kernel/main.c
+++ kernel/main.c
@@ -9,11 +9,6 @@ int main(void){
     put_str("hello kernel!\n");
     init_all();
 
-    // void* addr=get_kernel_pages(3);
-    // put_str("\n get_kernel_page start vaddr is ");
-    // put_int((uint32_t)addr);
-    // put_str("\n");
-
     thread_start("k_thread_a",31,k_thread_a,"argA ");
 
 
