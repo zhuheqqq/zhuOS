2cf61e14e9786e4fde083721a12ac2e1e0b4b29f
zhu-to-u <2528487925@qq.com>
2024-04-17 22:56:16 +0800

    创建页目录表+创建用户进程+虚拟地址位图

--- userprog/process.c
+++ userprog/process.c
@@ -1,7 +1,7 @@
 #include "process.h"
 #include "global.h"
 #include "debug.h"
-#include "memory.h"
+#include "../kernel/memory.h"
 #include "thread.h"    
 #include "list.h"    
 #include "tss.h"    
@@ -68,4 +68,52 @@ void process_activate(struct task_struct* p_thread){
         //更新该进程esp0,用于此进程被中断时保留上下文
         update_tss_esp(p_thread);
     }
+}
+
+//创建页目录表，将当前页表的表示内核空间的pde复制，成功返回页目录的虚拟地址，否则-1
+uint32_t* create_page_dir(void) {
+    //用户进程的页表不能让用户直接访问到，所以在内核空间来申请
+    uint32_t* page_dir_vaddr = get_kernel_pages(1);
+    if(page_dir_vaddr == NULL){
+        console_put_str("create_page_dir:get_kernel_page failed!");
+        return NULL;
+    }
+
+    //复制页表
+    memcpy((uint32_t*)((uint32_t)page_dir_vaddr + 0x300*4),(uint32_t*)(0xfffff000+0x300*4),1024);
+
+    //更新页目录地址
+    uint32_t new_page_dir_phy_addr = addr_v2p((uint32_t)page_dir_vaddr);
+    //页目录地址是存入页目录的最后一项，更新页目录地址为新页目录的物理地址
+    page_dir_vaddr[1023] = new_page_dir_phy_addr | PG_US_U | PG_RW_W | PG_P_1;
+
+    return page_dir_vaddr;
+}
+
+//创建用户进程的虚拟地址位图
+void create_user_vaddr_bitmap(struct task_struct* user_prog){
+    user_prog->userprog_vaddr.vaddr_start = USER_VADDR_START;
+    uint32_t bitmap_pg_cnt = DIV_ROUND_UP((0xc0000000 - USER_VADDR_START) / PG_SIZE / 8,PG_SIZE);
+
+    user_prog->userprog_vaddr.vaddr_bitmap.bits = get_kernel_pages(bitmap_pg_cnt);
+    user_prog->userprog_vaddr.vaddr_bitmap.btmp_bytes_len = (0xc0000000 - USER_VADDR_START) / PG_SIZE / 8;
+    bitmap_init(&user_prog->userprog_vaddr.vaddr_bitmap);
+}
+
+//创建用户进程
+void process_execute(void* filename, char* name){
+    //pcb内核的数据结构，由内核来维护进程信息,因此要在内核内存池中申请                                   
+    struct task_struct* thread = get_kernel_pages(1);
+    init_thread(thread, name, default_prio);
+    create_user_vaddr_bitmap(thread);
+    thread_create(thread, start_process, filename);
+    thread->pgdir = create_page_dir();
+
+    enum intr_status old_status = intr_disable();
+    ASSERT(!elem_find(&thread_ready_list,&thread->general_tag));
+    list_append(&thread_ready_list,&thread->general_tag);
+
+    ASSERT(!elem_find(&thread_all_list,&thread->all_list_tag));
+    list_append(&thread_all_list,&thread->all_list_tag);
+    intr_set_status(old_status);
 }
