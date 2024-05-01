f49294ad8f0a0adcc1b05bf08075271b383f6e06
zhu-to-u <2528487925@qq.com>
2024-05-01 20:00:45 +0800

    add file_create

--- fs/file.c
+++ fs/file.c
@@ -90,3 +90,89 @@ void bitmap_sync(struct partition* part, uint32_t bit_idx, uint8_t btmp) {
     }
     ide_write(part->my_disk, sec_lba, bitmap_off, 1);
 }
+
+//创建文件，成功返回文件描述符
+int32_t file_create(struct dir* parent_dir, char* filename, uint8_t flag) {
+    //公共缓冲区
+    void* io_buf = sys_malloc(1024);
+    if(io_buf == NULL) {
+        printk("in file_creat:sys_malloc for io_buf failed\n");
+        return -1;
+    }
+
+    uint8_t rollback_step = 0;//操作失败时回滚各资源状态
+
+    //为新文件分配inode
+    int32_t inode_no = inode_bitmap_alloc(cur_part);
+    if(inode_no == -1) {
+        printk("in file_creat:allocate inode failed\n");
+        return -1;
+    }
+
+    struct inode* new_file_inode = (struct inode*)sys_malloc(sizeof(struct inode));
+    if(new_file_inode == NULL) {
+        printk("file_creat:sys_malloc for inode failed\n");
+        rollback_step = 1;
+        goto rollback;
+    }
+    inode_init(inode_no, new_file_inode);//初始化i结点
+
+    //返回数组下标
+    int fd_idx = get_free_slot_in_global();
+    if(fd_idx == -1) {
+        printk("exceed max open files\n");
+        rollback_step = 2;
+        goto rollback;
+    }
+
+    file_table[fd_idx].fd_inode = new_file_inode;
+    file_table[fd_idx].fd_pos = 0;
+    file_table[fd_idx].fd_flag = flag;
+    file_table[fd_idx].fd_inode->write_deny = false;
+
+    struct dir_entry new_dir_entry;
+    memset(&new_dir_entry, 0, sizeof(struct dir_entry));
+
+    create_dir_entry(filename, inode_no, FT_REGULAR, &new_dir_entry);
+
+    //同步数据
+    //1. 在目录项下安装new_dir_entry目录项
+    if(!sync_dir_entry(parent_dir, &new_dir_entry, io_buf)) {
+        printk("sync dir_entry to disk failed\n");
+        rollback_step = 3;
+        goto rollback;
+    }
+
+    memset(io_buf, 0, 1024);
+
+    //2.将父目录i结点内容同步到硬盘
+    inode_sync(cur_part, parent_dir->inode, io_buf);
+    memset(io_buf, 0, 1024);
+
+    //3.将新创建的文件i结点内容同步到硬盘
+    inode_sync(cur_part, new_file_inode, io_buf);
+
+    //4.将inode_bitmap位图同步到硬盘
+    bitmap_sync(cur_part, inode_no, INODE_BITMAP);
+
+    //5.将创建的文件i结点添加到open_inodes链表
+    list_push(&cur_part->open_inodes, &new_file_inode->inode_tag);
+    new_file_inode->i_open_cnts = 1;
+
+    sys_free(io_buf);
+    return pcb_fd_install(fd_idx);
+
+    //某部失败则回滚
+rollback:
+    switch(rollback_step) {
+        case 3:
+            memset(&file_table[fd_idx], 0, sizeof(struct file));
+        case 2:
+            sys_free(new_file_inode);
+        case 1:
+        bitmap_set(&cur_part->inode_bitmap, inode_no, 0);
+        break;
+    }
+    sys_free(io_buf);
+    return -1;
+}
